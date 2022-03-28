3182f0237a0edbb7737ab7717602126f735b81e1
macbook <n.anastasiya020302@mail.ru>
2022-03-28 15:55:09 +0300

    initial commit

---
+++ src/main/java/com/anemogai/ace/underwear/community/entities/order/OrderItem.java
@@ -0,0 +1,19 @@
+package com.anemogai.ace.underwear.community.entities.order;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
