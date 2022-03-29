61ae330b73a478e721816b5c9305fc2ae0e8602a
macbook <n.anastasiya020302@mail.ru>
2022-03-29 17:53:04 +0300

    initial commit

--- src/main/java/com/anemogai/ace/underwear/community/entities/user/User.java
+++ src/main/java/com/anemogai/ace/underwear/community/entities/user/User.java
@@ -1,18 +1,21 @@
-package com.anemogai.ace.underwear.community.entities;
+package com.anemogai.ace.underwear.community.entities.user;
 
+import com.anemogai.ace.underwear.community.entities.cart.Cart;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
 import java.util.Set;
 
 @Entity
-@Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "users")
 public class User {
 
     @Id
@@ -29,8 +32,22 @@ public class User {
     @Column(name = "phone_number")
     private String phoneNumber;
 
+    @NotBlank
+    @Email
+    private String email;
+
     @ManyToMany(fetch = FetchType.EAGER)
     private Set<Role> roles;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn
+    private Cart cart;
+
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDate updatedAt;
 
     public User(String name, String password) {
         this.name = name;
