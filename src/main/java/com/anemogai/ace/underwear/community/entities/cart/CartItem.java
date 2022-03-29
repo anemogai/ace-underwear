65cd05e5948287a192da94ef7f221b4281811cfb
macbook <n.anastasiya020302@mail.ru>
2022-03-29 18:32:29 +0300

    initial commit

--- src/main/java/com/anemogai/ace/underwear/community/entities/cart/CartItem.java
+++ src/main/java/com/anemogai/ace/underwear/community/entities/cart/CartItem.java
@@ -1,6 +1,5 @@
 package com.anemogai.ace.underwear.community.entities.cart;
 
-import com.anemogai.ace.underwear.community.entities.product.Category;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,10 +21,9 @@ public class CartItem {
     @Column(name = "product_id")
     private Long productId;
 
-    @Column(name = "cart_id")
-    private Long cartId;
-
-    private Float price;
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
 
     private Integer quantity;
 
@@ -34,20 +32,4 @@ public class CartItem {
 
     @Column(name = "updated_at")
     private LocalDate updatedAt;
-
-    @ManyToOne
-    @JoinColumn(name = "cart_id")
-    private Cart cart;
-
-    public CartItem(Float price, Integer quantity) {
-        this.price = price;
-        this.quantity = quantity;
-    }
-
-    public CartItem(Float price, Integer quantity, LocalDate createdAt, LocalDate updatedAt) {
-        this.price = price;
-        this.quantity = quantity;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
 }
